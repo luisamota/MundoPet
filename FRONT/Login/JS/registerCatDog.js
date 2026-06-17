94a7af56f405d9a0aa1888792b40e1d0f9085f88
ANDRE LUIZ CANTERI FRUTUOSO <andre.lcfrutuoso@senacsp.edu.br>
2026-06-17 17:24:55 -0300

    As telas do Petshop

--- FRONT/Login/JS/registerCatDog.js
+++ FRONT/Login/JS/registerCatDog.js
@@ -1,31 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Seleciona os elementos do HTML
     const btnDog = document.querySelector(".btn-dog");
     const btnCat = document.querySelector(".btn-cat");
     const btnClose = document.querySelector(".close-btn");
 
-    // Evento de clique no botão Cachorro
     if (btnDog) {
         btnDog.addEventListener("click", () => {
+            localStorage.setItem("especieSelecionada", "cachorro");
             window.location.href = "../HTML/register.html";
         });
     }
 
-    // Evento de clique no botão Gato
     if (btnCat) {
         btnCat.addEventListener("click", () => {
+            localStorage.setItem("especieSelecionada", "gato");
             window.location.href = "../HTML/register.html";
         });
     }
 
-    // Evento de clique no botão Fechar (X)
     if (btnClose) {
         btnClose.addEventListener("click", () => {
-            // Se a index estiver na mesma pasta:
             window.location.href = "../HTML/home.html"; 
-            
-            // NOTA: Se a sua index estiver uma pasta para trás (junto com a pasta HTML), 
-            // mude a linha de cima para: window.location.href = "../index.html";
         });
     }
 });

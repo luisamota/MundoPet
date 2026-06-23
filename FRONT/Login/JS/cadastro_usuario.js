19fb7df22e735c85b443ea5df7626586d2a0f2be
ANDRE LUIZ CANTERI FRUTUOSO <andre.lcfrutuoso@senacsp.edu.br>
2026-06-23 17:14:56 -0300

    Atualização do Front

---
+++ FRONT/Login/JS/cadastro_usuario.js
@@ -0,0 +1,62 @@
+// 1. IMPORTANTE: Adicionamos a importação da URL do Render bem na primeira linha
+import { baseUrl } from './config.js';
+
+var btnCadastrar = document.getElementById("btn-cadastrar");
+
+btnCadastrar.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    var frmCadastro = document.querySelector("#frmCadastro");
+
+    const emailValor = frmCadastro.querySelector("#email").value;
+    const resultadoEmail = validarEmail(emailValor);
+
+    if (!resultadoEmail.valido) {
+        alert(resultadoEmail.mensagem);
+        return;
+    }
+
+    const especieSalva = localStorage.getItem("especieSelecionada");
+
+    if (!especieSalva) {
+        alert("Por favor, selecione se o seu pet é um cachorro ou um gato antes de preencher o cadastro.");
+        window.location.href = "../HTML/registerCatDog.html";
+        return;
+    }
+
+    var usuario = {
+        email: emailValor,
+        nome_usuario: frmCadastro.querySelector("#tutor-name").value,
+        nomePet: frmCadastro.querySelector("#petName").value,
+        especie: especieSalva,
+        raca: frmCadastro.querySelector("#breed").value,
+        senha: frmCadastro.querySelector("#confirm-password").value
+    };
+
+    // 2. MUDANÇA AQUI: Tiramos o 'getBaseUrl()' e usamos o `${baseUrl}` direto.
+    // Agora o JavaScript vai juntar "https://mundopet.onrender.com" com "/usuario/cadastrar"
+    fetch(`${baseUrl}/usuario/cadastrar`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(usuario)
+    })
+    .then(async response => {
+        if (!response.ok) {
+            const dadosErro = await response.json();
+            throw new Error(dadosErro.erro || "Erro ao cadastrar usuário");
+        }
+        return response.json();
+    })
+    .then(dados => {
+        console.log("Usuário salvo no banco:", dados);
+        alert(dados.mensagem);
+        frmCadastro.reset();
+        localStorage.removeItem("especieSelecionada");
+    })
+    .catch(error => {
+        console.error(error);
+        alert(error.message);
+    });
+});

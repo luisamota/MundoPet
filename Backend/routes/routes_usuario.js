694e2cf1a6154a64b7066fe50eef39f1ce97ebea
Andre Luiz Canteri <dedefrutuoso@gmail.com>
2026-06-24 00:08:15 -0300

    codigo atualizado

--- Backend/routes/routes_usuario.js
+++ Backend/routes/routes_usuario.js
@@ -6,7 +6,7 @@ const verificarAdmin = require('../middleware/verificaradmin');
 
 router.post("/cadastrar", usuarioController.cadastroUsuario);
 router.post("/admin/cadastro", usuarioController.cadastroAdmin);
-router.post("/", usuarioController.login);
+router.post("/login", usuarioController.login);
 router.post("/admin/login", usuarioController.loginAdmin);
 router.get("/", usuarioController.listarTodos);
 router.delete("/admin/excluir/:id", verificarAdmin, usuarioController.excluirUsuario);

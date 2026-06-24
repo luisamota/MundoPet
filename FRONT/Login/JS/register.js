88e654a37806b7edc1d62a4ea90584fff51b1afe
Andre Luiz Canteri <dedefrutuoso@gmail.com>
2026-06-23 23:35:12 -0300

    Codigo atualizado

--- FRONT/Login/JS/register.js
+++ FRONT/Login/JS/register.js
@@ -1,17 +1,3 @@
-const btnFechar = document.querySelector('.close-btn');
-const linkLogin = document.getElementById('link-login');
-
-linkLogin.addEventListener('click', (event) => {
-    event.preventDefault();
-    window.location.href = "../HTML/login.html";
-});
-
-if (btnFechar) {
-    btnFechar.addEventListener("click", () => {
-        window.location.href = "../HTML/registerCatDog.html";
-    });
-}
-
 import { baseUrl } from './config.js';
 
 // navegação
